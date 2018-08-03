faad4648c8f2a34625c1d578011f843a2312d248
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-03 17:44:56 +0530

    guess

--- m5/GuessMyNumber/guess_my_number.py
+++ m5/GuessMyNumber/guess_my_number.py
@@ -1,24 +1,25 @@
-#Guess My Number Exercise
+'''Guess My Number Exercise'''
 
 def main():
-   print('Think of a number between 0 to 100')
-   low=0
-   high=100
-   while True:
-       guess=(high+low)//2
-       user = input('Is your secret number ' + str(guess) + '\n'
-               + 'Enter "h" to indicate the guess is too high. ' + '\n'
-               + 'Enter "l" to indicate the guess is too low. ' + '\n'
-               + 'Enter "c" to indicate I guessed correctly. ')
-       if user == 'h':
-           low=guess
-       elif user == 'l':
-           high=guess
-       elif user == 'c':
-           print('Game over.Your secret number is :'+str(guess))
-           break
-       else:
-           print('Invalid entry')
-       guess=(high+low)//2
-if __name__== "__main__":
-   main()
+    """Hello time for guessing"""
+    print('Think of a number between 0 to 100')
+    low_v = 0
+    high_v = 100
+    while True:
+        guess = (high_v + low_v)//2
+        user = input('Is your secret number ' + str(guess) + '\n'
+                     + 'Enter "h" to indicate the guess is too high. ' + '\n'
+                     + 'Enter "l" to indicate the guess is too low. ' + '\n'
+                     + 'Enter "c" to indicate I guessed correctly. ')
+        if user == 'h':
+            low_v = guess
+        elif user == 'l':
+            high_v = guess
+        elif user == 'c':
+            print('Game over.Your secret number is :'+str(guess))
+            break
+        else:
+            print('Invalid entry')
+        guess = (high_v+low_v)//2
+if __name__ == "__main__":
+    main()
