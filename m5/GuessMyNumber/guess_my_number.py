1bca651450cad8ae406e99e8be7e32e624b08822
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-03 15:39:31 +0530

    final guess

--- m5/GuessMyNumber/guess_my_number.py
+++ m5/GuessMyNumber/guess_my_number.py
@@ -1,10 +1,24 @@
 #Guess My Number Exercise
 
 def main():
-	print("Please think a number between 1 and 100:")
-	LOW = 0
-	HIGH = 100
-	
-	
+   print('Think of a number between 0 to 100')
+   low=0
+   high=100
+   while True:
+       guess=(high+low)//2
+       user = input('Is your secret number ' + str(guess) + '\n'
+               + 'Enter "h" to indicate the guess is too high. ' + '\n'
+               + 'Enter "l" to indicate the guess is too low. ' + '\n'
+               + 'Enter "c" to indicate I guessed correctly. ')
+       if user == 'h':
+           low=guess
+       elif user == 'l':
+           high=guess
+       elif user == 'c':
+           print('Game over.Your secret number is :'+str(guess))
+           break
+       else:
+           print('Invalid entry')
+       guess=(high+low)//2
 if __name__== "__main__":
-	main()
+   main()
