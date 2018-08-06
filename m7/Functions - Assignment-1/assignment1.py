bdc53df5a515ed3bf2ccf4fde2da47acca73988a
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-06 22:37:41 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- m7/Functions - Assignment-1/assignment1.py
+++ m7/Functions - Assignment-1/assignment1.py
@@ -16,6 +16,6 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print(paying_debt_off_in_year(data[0], data[1], data[2]))
+    print("Remaining balance: " + str(paying_debt_off_in_year(data[0], data[1], data[2])))
 if __name__ == "__main__":
     main()
