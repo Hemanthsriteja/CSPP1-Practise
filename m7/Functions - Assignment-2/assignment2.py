4cb70ac4e177e89c397f93fa8cc93b34d46b7159
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-06 22:25:46 +0530

    " -> 0 of 10 passed. pylint: 10.00/10 "

--- m7/Functions - Assignment-2/assignment2.py
+++ m7/Functions - Assignment-2/assignment2.py
@@ -22,4 +22,4 @@ def main():
     data = list(map(float, data))
     print("The lowest payment is : " + str(paying_debtoffinayear(data[0], data[1])))
 if __name__ == "__main__":
-   main()
+    main()
