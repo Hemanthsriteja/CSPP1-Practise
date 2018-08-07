ba343a3ec313795cff1d7cea3b6a49b931515df6
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-07 15:22:20 +0530

    edit_gcd_iter

--- m8/GCD using Iteration/gcd_iter.py
+++ m8/GCD using Iteration/gcd_iter.py
@@ -1,20 +1,19 @@
 ''' Exercise: GCDIter
 # This function takes in two numbers and returns one number.'''
-def gcd_iter(a, b):
+def gcd_iter(a_v, b_v):
     '''
     a, b: positive integers
-    
     returns: a positive integer, the greatest common divisor of a & b.
     '''
     # Your code here
-    for i in range(min(a,b),0,-1):
-    	if a%i == b%i == 0:
-    		return i
-
+    for i in range(min(a_v, b_v), 0, -1):
+        if a_v%i == b_v%i == 0:
+            return i
 def main():
+    """def func"""
     data = input()
     data = data.split()
-    print(gcd_iter(int(data[0]), int(data[1]))) 
+    print(gcd_iter(int(data[0]), int(data[1])))
 
 if __name__ == "__main__":
     main()
