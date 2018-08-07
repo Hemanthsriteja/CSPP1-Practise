7ff72f92433aaa607a1060890a926123d9d4d9ae
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-07 12:39:24 +0530

    edit_power_iter

--- m8/power using Iteration/power_iter.py
+++ m8/power using Iteration/power_iter.py
@@ -1,32 +1,24 @@
 # Exercise: PowerIter
-# Write a Python function, iterPower(base, exp), that takes in two numbers and returns the base^(exp) of given base and exp.
-
-# This function takes in two numbers and returns one number.
+''' Write a Python function, iterPower(base, exp),finding base power exp'''
 
 
 def iter_power(base, exp):
     '''
     base: int or float.
     exp: int >= 0
- 
     returns: int or float, base^exp
     '''
-    # Your code here
-    f = 1
-    while exp !=0:
-        f = f*base
+    f_v = 1
+    while exp != 0:
+        f_v = f_v*base
         exp = exp-1
-    return f
-
-    
-
-
-
+    return f_v
 def main():
+    """defining main function"""
 
     data = input()
     data = data.split()
-    print(iter_power(float(data[0]),int(data[1])) )
+    print(iter_power(float(data[0]), int(data[1])))
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
