95d5d164872bb025e49d8fa6cd56c46a2a64e529
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-10 22:40:07 +0530

    division

---
+++ m11/Integer Division Exercise/integer_division.py
@@ -0,0 +1,25 @@
+#Exercise: Integer Division Exercise
+#Modify the code for integer_division so that this error does not occur.
+
+def integer_division(x_v, a_v):
+    """
+    x: a non-negative integer argument
+    a: a positive integer argument
+
+    returns: integer, the integer division of x divided by a.
+    """
+    count_v = 0
+    while x_v >= a_v:
+        count_v += 1
+        x_v = x_v - a_v
+    return count_v
+    
+
+def main():
+    data = input()
+    data = data.split()
+    print(integer_division(int(data[0]), int(data[1])))
+
+
+if __name__== "__main__":
+    main()
